02cf0934ae2374cfcdf17174f434f9af7dc90c6e
Luca Lamorte <lclamorte@gmail.com>
2022-01-09 10:54:11 -0300

    This is a new commit for what I originally planned to be amended

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
+let enlace = document.getElementsByClassName("active");
+
 
 boton.addEventListener("click", function(){
 	if (contador == 0){
 		enlaces.className = ('enlaces dos');
-		contador=1;
-	}else{
+		contador = 1;
+	}
+	else{
 		enlaces.classList.remove('dos');
 		enlaces.className = ('enlaces uno');
 		contador = 0;
 	}
 })
+
+for (var i = 0 ; i < enlace.length; i++){
+	enlace[i].addEventListener('click', function(){
+		if (contador != 0){
+			enlaces.classList.remove('dos');
+			enlaces.className = ('enlaces uno');
+			contador = 0;
+		}
+	})
+}
