4b8374b01b23b1658f58e055d86c5cf9c6b5a663
Luca Lamorte <86692563+lucalamorte@users.noreply.github.com>
2022-01-10 02:43:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
 let contador = 0;
@@ -55,47 +54,3 @@ for (var i = 0 ; i < enlace.length; i++){
 		}
 	})
 };
-=======
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
-let enlace = document.getElementsByClassName("active");
-
-const menuBtn = document.querySelector('.icono');
-let menuOpen = false;
-
-/*Animación: abrir/cerrar menú responsive al clickear ícono*/
-boton.addEventListener("click", function(){
-	if (contador == 0){
-		enlaces.className = ('enlaces dos');
-		contador = 1;
-	}
-	else{
-		enlaces.classList.remove('dos');
-		enlaces.className = ('enlaces uno');
-		contador = 0;
-	}
-});
-
-/*Animación: cerrar menú responsive al clickear link*/
-for (var i = 0 ; i < enlace.length; i++){
-	enlace[i].addEventListener('click', function(){
-		if (contador != 0){
-			enlaces.classList.remove('dos');
-			enlaces.className = ('enlaces uno');
-			contador = 0;
-		}
-	})
-};
-
-/*Animación: menú hamburguesa que se transforma en X*/
-menuBtn.addEventListener('click', () => {
-	if (!menuOpen) {
-		menuBtn.classList.add('open');
-		menuOpen = true;
-	} else {
-		menuBtn.classList.remove('open');
-		menuOpen = false;
-	}
-});
->>>>>>> d46aae8aa9be6305b4e09623166e25cf45ad5241
